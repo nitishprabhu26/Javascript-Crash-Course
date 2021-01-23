935d8a25fe5507461dc88db279290af3668a4f67
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-01-23 11:35:14 -0600

    Variables

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 "# Javascript-Crash-Course" 
+
+# JavaScript Crash Course For Beginners
+https://www.youtube.com/watch?v=hdI2bqOjy3c
---
+++ main.js
@@ -0,0 +1,22 @@
+// LOGGING OUTPUT
+// alert('Hello World'); 
+// Do not use for debugging. Stops script and only strings
+
+// https://developer.mozilla.org/en-US/docs/Web/API/console
+// console.log("HI !")
+// console.error("This is an error")
+// console.warn("This is a warning")
+// https://developer.mozilla.org/en-US/docs/Web/API/console
+
+
+// VARIABLES - var, let, const
+
+// Var- globally scoped
+// let, const- globally scoped
+// Const - cant reassign value, use only if you wont reassign value, 
+
+let age = 30;
+// let can be re-assigned, const can not
+age = 31;
+
+
