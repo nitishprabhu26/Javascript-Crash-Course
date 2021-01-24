7077cfd10fbfc120cc97e38a592b6ee78ed8d953
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-01-24 04:52:02 -0600

    element selector

---
+++ 010_element_selectors.js
@@ -0,0 +1,36 @@
+// ELEMENT SELECTORS
+
+// parent object of the browser
+console.log(window)
+
+// Single Element Selectors - selects only the first one
+console.log(document.getElementById('my-form'));
+console.log(document.querySelector('.container'));
+console.log(document.querySelector('h1'));
+
+// Multiple Element Selectors
+
+// can use id, class, tag anything - new one - returns nodelist - can use array methods on this
+console.log(document.querySelectorAll('.item'));
+
+// old - HTML Collection - cant use array methods on this - need to convert it manually to use array methods
+console.log(document.getElementsByTagName('li'));
+console.log(document.getElementsByClassName('item'));
+
+const items = document.querySelectorAll('.item');
+items.forEach((item) => console.log(item));
+
+
+// MANIPULATING THE DOM
+const ul = document.querySelector('.items');
+// ul.remove();
+// ul.lastElementChild.remove();
+ul.firstElementChild.textContent = 'Hello';
+ul.children[1].innerText = 'Nitish';
+ul.lastElementChild.innerHTML = '<h1> WELCOME !!</h1>';
+
+const btn = document.querySelector('.btn');
+btn.style.background = 'red';
+
+
+ul.remove();
