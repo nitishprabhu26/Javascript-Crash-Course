bbcbe684c70f16e1d7f2f301acaf84e07bc42ff0
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-01-24 05:01:14 -0600

    Events

---
+++ 011_events.js
@@ -0,0 +1,17 @@
+// EVENTS
+
+// Mouse Event
+// other events - mouseover, mouseout ets
+// btn.addEventListener('click', e => {
+//   e.preventDefault();
+//   console.log(e.target.className);
+//   document.getElementById('my-form').style.background = '#ccc';
+//   document.querySelector('body').classList.add('bg-dark');
+//   ul.lastElementChild.innerHTML = '<h1>Changed</h1>';
+// });
+
+// Keyboard Event
+// const nameInput = document.querySelector('#name');
+// nameInput.addEventListener('input', e => {
+//   document.querySelector('.container').append(nameInput.value);
+// });
