ac4bf2a74b5e5da65e5195b3bfdf216509bc88fe
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-06-20 02:55:59 -0500

    higher order functions - foreach and filter

--- 011_events.js
+++ 011_events.js
@@ -2,16 +2,16 @@
 
 // Mouse Event
 // other events - mouseover, mouseout ets
-// btn.addEventListener('click', e => {
-//   e.preventDefault();
-//   console.log(e.target.className);
-//   document.getElementById('my-form').style.background = '#ccc';
-//   document.querySelector('body').classList.add('bg-dark');
-//   ul.lastElementChild.innerHTML = '<h1>Changed</h1>';
-// });
+btn.addEventListener('click', e => {
+  e.preventDefault();
+  console.log(e.target.className);
+  document.getElementById('my-form').style.background = '#ccc';
+  document.querySelector('body').classList.add('bg-dark');
+  ul.lastElementChild.innerHTML = '<h1>Changed</h1>';
+});
 
 // Keyboard Event
-// const nameInput = document.querySelector('#name');
-// nameInput.addEventListener('input', e => {
-//   document.querySelector('.container').append(nameInput.value);
-// });
+const nameInput = document.querySelector('#name');
+nameInput.addEventListener('input', e => {
+  document.querySelector('.container').append(nameInput.value);
+});
